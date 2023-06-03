6b079fdb155d895af9b71b85a4fbf987e29c246c
Miguel <tonycortesano@gmail.com>
2023-06-02 19:44:17 -0500

    initial commit

---
+++ src/test/java/firebase_auth_example/FirebaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package firebase_auth_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
